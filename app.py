a295fd5cf06394a77cfe77347d5b61b8da07d071
sine <963366202@qq.com>
2018-03-17 10:32:37 +0800

    more data

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from fuzzywuzzy import process
+import sqlite3 as sq3
+
+vscode = ['vscode', 'vs code', 'visual studio code', 'code']
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def main():
+    # print(request.form.to_dict())
+    action = request.form.get('action', 'none')
+    software = request.form.get('software', 'none')
+    if software in vscode:
+        software = vscode[0]
+    
+    conn = sq3.connect('sheets.db')
+    c = conn.cursor()
+    table = list(c.execute('select action, key from vscode'))
+    action = process.extractOne(action, table[:][0])[0]
+    for row in table:
+        if row[0] == action:
+            res = row[1]
+            break
+    res = '{"data":"' + res + '"}'
+    return res
+
+if __name__ == '__main__':
+    app.run(debug=True)
